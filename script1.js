8bfaff746db8b9f7d7a27838796953b78d3eba59
Sneh Anna Philip <35308121+SnehPhilip@users.noreply.github.com>
2026-03-24 08:33:33 +0530

    added 2nd index page and script page

--- script1.js
+++ script1.js
@@ -2,8 +2,6 @@ function loadPage(pageNumber) {
     // Note: This always targets the first 'image-results-area' [cite: 12, 42]
     const displayArea = document.getElementById('image-results-area');
 
-
-
     if (!displayArea) {
         console.error("Target area not found!");
         return;
@@ -53,53 +51,51 @@ function loadPage(pageNumber) {
     if (pageNumber == 11) {
 
 
-    toggle_on(11)
-    bg_off(12)
+    toggle_on(11);    bg_off(12);    bg_off(13);    bg_off(21);    bg_off(22);    bg_off(23)
     
         displayArea.innerHTML = projectTemplate(
-            "University Exam Automation | Software Developer"," ",
-            "To modernize the University Exam Section's manual workflows into a synchronized, enterprise-grade web application.",
-            "Python, Django, PostgreSQL, NAD Digilocker API",
+            " Data Analyst | Freelance | NDVI Calculation and Time Series Analysis for Petroleum infrastructure "," ",
+            "Normalized Vegetation Index Calculation and Time series Analysis using Google Earth Engine (GEE)- a massive, cloud-based platform designed for geospatial analysis",
+            "javascript , global elevation dataset ,LANDSAT/LC08/C02/T1_L2 dataset",
             `<ul>
-                <li>Designed custom Python-based NAD formatting tool for Digilocker synchronization.</li>
-                <li>Implemented Cumulative Credit tracking for B.Tech promotion logic.</li>
-                <li>Re-engineered Degree Certificate modules with strict validation protocols.</li>
-                <li>Developed automated CML reporting.</li>
+                <li>Analyse the cloud-based platform designed for geospatial planetary scale analysis.A unique spectral signature - raw reflectance values in two specific bands is fundamental for calculation- Red Light  ,Near-Infrared Light (NIR) </li><li> Calculate NDVI for specific time period and geographical location. </li>
+                <li>Java script code development for normalized Calculation and Time Series Analysis</li>
+               
             </ul>`
         );
     } 
     else if (pageNumber == 12) {
 
-    bg_off(11)
-    bg_off(13)
-    bg_off(21)
-    bg_off(22)
-    bg_off(23)
-    toggle_on(12)
+    bg_off(11);    bg_off(13);    bg_off(21);    bg_off(22);    bg_off(23);    toggle_on(12)
         displayArea.innerHTML = projectTemplate(
-            "SAP Hybris Customization | Software Developer"," ",
-            "Customizing e-commerce frameworks to drive European sales performance.",
-            "J2EE, Spring MVC, SAP Hybris, Oracle ATG",
+            "Aggressive Sports Actions Detection  | HAR Research Project"," ",
+            `<li>Detection of Aggressive Sports Actions (Violence and Basketball-slam dunks) using 3dCNN.</li>`,
+            " Datasets explored (NBA Basketball, SpaceJam, and UCF101 dunk subset),Hockey Fight Dataset (used for violence detection benchmarks)",
             `<ul>
-                <li>Provided DevOps support on the Oracle ATG platform.</li>
-                <li>Customized enterprise e-commerce modules using J2EE Spring MVC.</li>
+                <li>Background : Evolution analysis of computer vision in sports science and analysis of different data modalities</li>
+                <li>3D Convolutional Neural Networks, demonstrate their capability of extracting spatio-temporal features from videos, being effective in violence detection.</li>
+                <li>The implementation : based on 3dCNN feature extractor and artificial neural network classifier.Frame Extraction ,Downsampling ,Resizing ,Spatial Augmentation ,Temporal Augmentation,3D Kernels that slide across height, width, and time to identify spatio-temporal features , Global Average Pooling layer and Fully Connected Dense layer to transition from feature maps to final class predictions.
+                </li>
+                
+                <li><h3>Project Documentation</h3>
+    <p>Click the link below to view the full document:</p>
+    <a href="https://docs.google.com/document/d/1KDSfZoLGET6aEip0MfiQfD730wlajQlUBPaFwQgPZ-E/edit?usp=sharing" target="_blank">
+        Open Google Doc
+    </a>
+                </li>
             </ul>`
         );
     }
     else if (pageNumber == 13) {
-    bg_off(11)
-    bg_off(12)
-    bg_off(21)
-    bg_off(22)
-    bg_off(23)
-    toggle_on(13)
+    bg_off(11);   bg_off(12);    bg_off(21);    bg_off(22);    bg_off(23);    toggle_on(13)
         displayArea.innerHTML = projectTemplate(
-            "Banking Backend Automation | Sr. Software Engineer","project title ",
-            "Automating backend processes and maintaining high-integrity SQL interactions.",
-            "J2EE, Spring MVC, SQL",
+            "Image Classification Models | IEEE, RWEP Workbench ","Neural Networks",
+            "Deep Learning models to classify images - Evaluating Classifier Efficacy",
+            "ANN(Flattened),CNN,Deep CNN ,Transfer Learning",
             `<ul>
-                <li>Developed web pages enabling backend automation.</li>
-                <li>Managed meticulous SQL interactions to maintain database integrity.</li>
+                <li>Advanced Image Classification via Transfer Learning using ResNet50 and VGG16 with pre-trained weights </li>
+
+                <li>Comparative Analysis of Model Performance on Fashion Image Datasets with ANN ,CNN  </li>
             </ul>`
         );
     }
@@ -113,15 +109,12 @@ function loadPage(pageNumber) {
             //btn23.style.color = btn22.style.color = btn13.style.color = btn12.style.color = "black";
         displayArea.innerHTML = projectTemplate(
             "Software Developer | Aris Lab, CUSAT ","CBCS Project, Cochin University Exam Section",
-            "Automating workflows for diverse schemes with timely academic mandates, facilitating 13-tier granular role-based access control for administrative officers, fostering interdisciplinary and holistic education for user groups such as UG, PG, Diploma, and Ph.D. students.",
+            "To automate choice based credit system , designed to move away from a rigid, one-size-fits-all curriculum toward a more flexible student-centric model.Support Interdisciplinary choice , Cumulative Grade Point Average , portable credits etc to maintain the modern academic framework.",
             "Python 3.5,django 1.11.6, Pycharm,Postgresql PgAdmin4,git & BitBucket, HTML,CSS,BootStrap,JQuery ",
             `<ul>
-                <li>Designed custom Python-based NAD formatting tool for Digilocker synchronization.</li>
-                <li>Implemented Cumulative Credit tracking for B.Tech promotion logic.</li>
-                <li>Developed automated Consolidated MArk List reporting.</li>
-                <li>Re-engineered the Degree Certificate and Mark Card modules with protocols that prevented certificate generation for students with pending results</li>
-                <li>...</li>
-                
+            <li>Automate workflows for diverse schemes facilitating 13-tier granular role-based access control for administrative officers.</li>
+            <li>Engineer and deploy new modules for the high-traffic statewide enterprise application, maintaining functionality of the end-to-end academic lifecycle based on complex university norms and national compliance standards.
+</li><li>Update timely academic mandates, fostering interdisciplinary and holistic education for user groups such as UG, PG, Diploma, and Ph.D. students</li><li>...</li>
             </ul>`
         );
     }
@@ -132,12 +125,12 @@ function loadPage(pageNumber) {
             //btn22.style.color = "blue";
             //btn23.style.color = btn21.style.color = btn13.style.color = btn12.style.color = "black";
         displayArea.innerHTML = projectTemplate(
-            "Software Developer | Mindcurv (Accenture Song)","Nikon ,Deli XL e-commerce project","Major enterprise-level e-commerce initiatives. Operates relying on robust, scalable backends like SAP Hybris (SAP Commerce Cloud) to manage complex product catalogs, B2B/B2C workflows, and global supply chain integrations.",
+            "Software Developer | Mindcurv (Accenture Song)","Nikon , Deli XL e-commerce projects","Web app development for major enterprise-level E-COMMERCE initiatives. Customize robust, scalable backends like SAP Hybris (SAP Commerce Cloud) to manage complex product catalogs, B2B/B2C workflows,Promotion Rules built on Drools Engine and Impex Scripts.",
             "J2EE Spring MVC, SAP Hybris, Oracle ATG",
             `<ul>
                 <li>Managed J2EE and Spring MVC customizations for Nikon’s pan-European Online Sales platform</li>
                 <li>Deployed business logic within the SAP Hybris framework to meet regional regulatory requirements.</li>
-                <li>Enhanced platform stability for Oracle ATG-based site resolving critical production bugs in collaboration with DeliXL DevOps team.</li>
+                <li>Enhanced platform stability in collaboration with DeliXL DevOps team for Oracle ATG-based site, resolving critical production bugs </li>
                 <li>...</li>
             </ul>`
         );
@@ -150,11 +143,13 @@ function loadPage(pageNumber) {
         //btn22.style.color = btn21.style.color = btn13.style.color = btn12.style.color = "black";
         displayArea.innerHTML = projectTemplate(
             "Sr Software Engineer | Tech Mahindra","Online SBI Banking Project - Retail,Corporate,DEMAT accounts ",
-            " Develop high-throughput logic for retail and bulk financial transactions using J2EE Spring MVC with SQL at backend, to serve Retail,Corporate and Demat account holders of the bank.",
+            `To develop high-throughput response for nation-wide banking operations for high-volume Retail and Corporate bulk financial transactions.
+            Management of Demat accounts for seamless integration with depository participants (CDSL/NSDL).  
+            Scalable Backend Design: Engineered modules in the application layer using J2EE Spring MVC with SQL at backend and jsp pages at frontend .`,
             "J2EE, jsp pages , Spring MVC, SQL",
             `<ul>
                 <li>Backend automation via Spring Model View Controller architecture rendered on Java Server Pages.</li>
-                <li>Orchestrating precise SQL execution to uphold ledger consistency and data integrity across critical banking infrastructures</li>
+                <li>Orchestrated precise SQL execution to uphold ledger consistency and data integrity across critical banking infrastructures</li>
                 <li>Automation: Engineered  General Dealer Finance (GDF) lifecycle and EzInstructions for Demat account holders.</li>
                 <li>Supported  Online Tax Accounting module(OLTAS) ensuring precise real-time data capture and tracking  with Challan Identification Number</li>
                 <li>Enhanced Electronic Clearing System and Bulk Transaction modules</li>
@@ -166,26 +161,24 @@ function loadPage(pageNumber) {
 }
 function toggle_on(id)
 {   
-
 const button = document.getElementById("btn" + id);
 const element = document.getElementById("proj" + id); 
 
     // This hides the button and collapses the space it took
     button.style.display = "none";
-    element.style.backgroundColor = "#e7f3ff"; // Soft blue highlight
-    element.style.padding = "10px";           // Optional: makes the color look better
+    element.className = "bg-info bg-opacity-10 p-4 border rounded-3 shadow-sm";
     }
 function bg_off(id)
 {   
-
 const button = document.getElementById("btn" + id);
 const element = document.getElementById("proj" + id);   
     
     button.style.display = "inline-block";
     element.style.backgroundColor = ""; 
+    element.className = "";
     //element.style.padding = "10px";           // Optional: makes the color look better
     }
     
 function closeProject() {
-    document.getElementById('image-results-area').innerHTML = '<h6 class="text-secondary text-uppercase mb-4" style="font-size: 0.75rem; letter-spacing: 1px;">Detailed Overview</h6><p>Loading initial batch...</p>';
+    document.getElementById('image-results-area').innerHTML = '<h6 class="text-secondary text-uppercase mb-4" style="font-size: 0.75rem; letter-spacing: 1px;">Detailed Overview</h6><p>Click Read More>> button to view each Project Detail. </p>';
 }
